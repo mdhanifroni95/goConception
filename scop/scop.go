ea9a65b284134368090f07d67d9cda55e20f59f6
mdhanifroni95 <eng.hanif.roni63@gmail.com>
2026-06-10 20:57:31 +0600

    Add initial implementation of switch statement examples in main function

--- scop/scop.go
+++ scop/scop.go
@@ -1,5 +1,6 @@
 package main
 
+// Scope in Go refers to the visibility and lifetime of variables, functions, and other identifiers. It determines where these identifiers can be accessed and modified within a program. Go has several types of scopes, including package scope, function scope, block scope, and file scope.
 import "fmt"
 
 // var (
@@ -12,14 +13,31 @@ import "fmt"
 // 	printNum(res)
 // }
 
-func main() {
-	func(a int, b int) {
-		c := a + b
-		fmt.Println("This is an anonymous function in scop package", c)
-		printNum(c)
-	}(9, 8)
-}
+// func main() {
+// 	func(a int, b int) {
+// 		c := a + b
+// 		fmt.Println("This is an anonymous function in scop package", c)
+// 		printNum(c)
+// 	}(9, 8)
+// }
 
-func printNum(res int) {
-	fmt.Println("This is a function in scop package", res)
+// func printNum(res int) {
+// 	fmt.Println("This is a function in scop package", res)
+// }
+
+//package scope
+var appName = "My App"
+
+func main() {
+	//function scope
+	name := "John Doe"
+
+	//block scope
+	if age := 20; age > 18 {
+		fmt.Println(age)
+	}
+	// fmt.Println(age) // This will cause an error because age is not accessible outside the if block
+
+	fmt.Println(name)
+	fmt.Println(appName)
 }
